9d66226ad736348de0238c5e26922c3026316ad0
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2021-06-03 18:07:30 +0530

    added cache in view

--- shelf/views.py
+++ shelf/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render
+from django.core.cache import cache
 
 from .shelf import get_book_list_from_shelf
 
 
 def shelf_view(request, shelf):
     template = "shelf/shelf.html"
-    book_list = get_book_list_from_shelf(shelf=shelf)
+    cached_list = cache.get(shelf, None)
+    book_list = cached_list
+    if cached_list is None:
+        book_list = get_book_list_from_shelf(shelf=shelf)
+        cache.set(shelf, book_list)
     context = {
         "shelf_name": shelf,
         "book_list": book_list
